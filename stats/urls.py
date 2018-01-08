50f68197f79a99d8c404d478363c4e423f965322
larryworm1127 <shilerong@gmail.com>
2018-01-08 14:40:05 -0500

    edited template urls, and changed container type

--- stats/urls.py
+++ stats/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.main_page),
-    url(r'^all_team_statistics/$', views.all_team_stats),
-    url(r'^box_score/$', views.box_score),
-    url(r'^view_standing/(?P<season>[0-9-]+)$', views.standing),
-    url(r'^graphs/$', views.stats_graph, name='index'),
+    url(r'^$', views.main_page, name='main_page'),
+    url(r'^all_team_statistics/$', views.all_team_stats, name='all_team_stats'),
+    url(r'^box_score/$', views.box_score, name='box_score'),
+    url(r'^view_standing/(?P<season>[0-9-]+)$', views.standing, name='standing'),
+    url(r'^graphs/$', views.stats_graph, name='graphs'),
     url(r'^graphs/bar_wlr_chart', views.bar_wlr_chart, name='bar_wlr_chart'),
     url(r'^graphs/bar_points_chart', views.bar_points_chart, name='bar_points_chart'),
     url(r'^graphs/bar_rebounds_chart', views.bar_rebounds_chart, name='bar_rebounds_chart'),
     url(r'^graphs/bar_assists_chart', views.bar_assists_chart, name='bar_assists_chart'),
-    url(r'^view_team_comparisons/$', views.team_comparisons),
+    url(r'^view_team_comparisons/$', views.team_comparisons, name='team_comparisons'),
 ]
--- tournament/urls.py
+++ tournament/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from tournament import views
 
 urlpatterns = [
-    url(r'^api/(?P<season>[0-9-]+)$', views.tournament),
+    url(r'^api/(?P<season>[0-9-]+)$', views.tournament, name='api'),
     url(r'^bracket/$', views.bracket),
-    url(r'^bracket/(?P<season>[0-9-]+)$', views.bracket),
+    url(r'^bracket/(?P<season>[0-9-]+)$', views.bracket, name='tournament_bracket'),
     url(r'^bracket/simulate_playoff$', views.simulate_playoff),
     url(r'^bracket/simulate_season$', views.simulate_season)
 ]
