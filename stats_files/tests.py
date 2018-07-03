4ee33dde7a568e67a57ab902e78ddcd00efae28d
larryworm1127 <shilerong@gmail.com>
2018-07-02 21:51:43 -0400

    added condition to check for CI env variable for

--- stats_files/tests.py
+++ stats_files/tests.py
@@ -1,27 +1,26 @@
 import unittest
+from os import environ
 
 from stats_files import create_player_rating, create_team_list, get_id_from_abb, create_other_files
 
-"""Commented out to avoid CI timeout, needs to wait until nba_py is fixed
-class TestMain(unittest.TestCase):
-    def test_create_team_list(self):
-        self.assertTrue(create_team_list())
+if not environ['CI']:
+    class TestMain(unittest.TestCase):
+        def test_create_team_list(self):
+            self.assertTrue(create_team_list())
 
-    def test_get_id_from_abb(self):
-        self.assertEqual(get_id_from_abb('GSW'), '1610612744')
-"""
+        def test_get_id_from_abb(self):
+            self.assertEqual(get_id_from_abb('GSW'), '1610612744')
+
+
+    class TestFileCheck(unittest.TestCase):
+        def test_init(self):
+            create_other_files.init()
 
 
 class TestCreatePlayerRating(unittest.TestCase):
     def test_sort_player_into_team(self):
         self.assertTrue(create_player_rating.sort_player_into_team())
 
-
-"""Commented out to avoid CI timeout, needs to wait until nba_py is fixed
-class TestFileCheck(unittest.TestCase):
-    def test_init(self):
-        create_other_files.init()
-"""
 
 if __name__ == '__main__':
     unittest.main()
